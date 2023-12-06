0003d003e5dd3d9b86525e5e53b6b0db1c1942c9
Alyona Kolgasheva <kolgasheva065@gmail.com>
2023-12-06 02:46:55 +0200

    add anim on title

--- src/components/Title/Title.vue
+++ src/components/Title/Title.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="kinesis-container__title">
   <h3 :class="getClassObject()">
       <span class="title--pink">
         [
@@ -6,34 +7,46 @@
     {{ title }}
     <span class="title--pink"> ]</span>
   </h3>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "FilterItem",
-  props: {
-    class: {
-      type: String,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-  },
-  methods: {
-    getClassObject() {
-      const classes = {
-        'title': true,
-      };
+<script setup>
 
-      if (this.class) {
-        classes[this.class] = true;
-      }
+import {defineProps, ref, onMounted, onBeforeUnmount, computed} from 'vue';
 
-      return classes;
-    },
-  },
+const props = defineProps(['class', 'title']);
+
+let isAnimActive = ref(false)
+function getClassObject() {
+  const classes = {
+    title: true,
+    'init': isAnimActive.value,
+  };
+
+  if (props.class) {
+    classes[props.class] = true;
+  }
+
+  return classes;
 }
+
+const handleScroll = () => {
+  isAnimActive.value = true;
+
+  setTimeout(() => {
+    isAnimActive.value = false;
+  }, 1000);
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+// Видаляємо слухач подій при знищенні компонента
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
+
 </script>
 
 <style lang="scss" scoped src="./style.scss"></style>
