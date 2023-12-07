27b358567886705d194e8587100204c57bb8167f
Alyona Kolgasheva <kolgasheva065@gmail.com>
2023-12-07 15:42:17 +0200

    fix experience

--- src/components/ExperienceSection/ExperienceSection.vue
+++ src/components/ExperienceSection/ExperienceSection.vue
@@ -7,12 +7,20 @@
     <div class="experience-item">
       <h5 class="experience-item__title">Freelance</h5>
       <p class="experience-item__time-interval">October 2023 - Present</p>
+      <ul class="project-list">
+        <li class="project-item">
+          1. Creating an adaptive portfolio website using Vue.js;
+        </li>
+        <li class="project-item">
+          2. Implementing interactivity using JavaScript and developing an adaptive website layout.
+        </li>
+      </ul>
       <ul class="tasks-list">
-        <li class="task-item">&#8226; Creating an adaptive portfolio website using Vue.js;</li>
-        <li class="task-item">&#8226; Using JavaScript to create an additional interactivity on the site;</li>
-        <li class="task-item">&#8226; Development of an adaptive website layout that seamlessly adjusts to screens of various sizes is underway;</li>
-        <li class="task-item">&#8226; Experience with HTML5 and SCSS;</li>
-        <li class="task-item">&#8226; Setting up project with the Gulp builder and npm package manager.</li>
+        <li class="project-item task-item">&#8226; Creating an adaptive portfolio website using Vue.js;</li>
+        <li class="project-item task-item">&#8226; Using JavaScript to create an additional interactivity on the site;</li>
+        <li class="project-item task-item">&#8226; Development of an adaptive website layout that seamlessly adjusts to screens of various sizes is underway;</li>
+        <li class="project-item task-item">&#8226; Experience with HTML5 and SCSS;</li>
+        <li class="project-item task-item">&#8226; Setting up project with the Gulp builder and npm package manager.</li>
       </ul>
     </div>
   </kinesis-container>
